37c856c22fc573bdae8334ed9505880db2fa77dc
Prashant Sharma <prash.shr@gmail.com>
2017-07-14 15:51:12 +0200

    Updated conf files

--- ssl/files/startup_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo chown -R nginxuser:nginxuser ${NGINX_INSTALL_DIR} /home/${NGINXUSER} /var/cache/ /var/log/
-sleep 7
-sudo cron
-sudo /etc/init.d/postfix start
-sudo ln -s ~/letsencrypt_renew.sh /etc/cron.weekly/letsencrypt_renew.sh
-sudo service nginx start
-sleep infinity
